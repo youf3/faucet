8f959ad406f186153f00668904ed43087b6ca4be
Se-young Yu <youff3@gmail.com>
2016-04-04 14:55:52 +1200

    added testbed setting script

---
+++ tests/testbed-setting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+bridge="br0"
+int1="eth2"
+int2="eth4"
+int3="eth5"
+int4="eth6"
+
+controller="tcp:127.0.0.1:6633"
+dpid="0000000024cf8e6c"
+
+
+ovs-vsctl add-br $bridge
+ovs-vsctl add-port $bridge $int1
+ovs-vsctl add-port $bridge $int2
+ovs-vsctl add-port $bridge $int3
+ovs-vsctl add-port $bridge $int4
+ovs-vsctl set-fail-mode $bridge secure
+ovs-vsctl set-controller $bridge $controller
+ovs-vsctl set bridge $bridge protocols=OpenFlow13
+ovs-vsctl set bridge $bridge other-config:datapath-id=$dpid
