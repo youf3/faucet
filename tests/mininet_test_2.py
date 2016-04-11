e79432ae3d3f6afeb5703fa994df6eca29b31ab3
Se-young Yu <youff3@gmail.com>
2016-04-12 10:01:42 +1200

    tidy up and added comment

--- tests/mininet_test_2.py
+++ tests/mininet_test_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# This program measures number of flows a switch can support until it start software switching
+#using mininet
 
 from mininet.net import Mininet
 from mininet.cli import CLI
@@ -9,9 +11,12 @@ import threading
 import time
 import math
 
-def max_flow_test(max_flows):
+# Specify interfaces that are connected to the target switch
+interface1 = 'eth2'
+interface2 = 'eth4'
+
+def max_flow_test(max_flows,num_iperf = 1):
     n =  int(2*(math.sqrt(max_flows/2)))
-    print('%s nodes generated' %n)
     net = Mininet( topo=None,build=None)
 
     s2 = net.addSwitch('s2',failMode='standalone')
@@ -29,8 +34,10 @@ def max_flow_test(max_flows):
             net.addLink(host, s3)
             subnet2.append(host)
 
-    Intf( 'eth2', node=s2 )
-    Intf( 'eth4', node=s3 )
+    print('%s nodes generated' %n)
+
+    Intf( interface1, node=s2 )
+    Intf( interface2, node=s3 )
 
     net.start()
     count = 0
@@ -38,19 +45,34 @@ def max_flow_test(max_flows):
     for host1 in subnet1:
         for host2 in subnet2:
             popens[host1] = host1.popen( "ping %s" % host2.IP() )
-            #print('ping %s -> %s' %(host1, host2))
             count += 1
 
-    print('%s connections generated '%count)
-    #for host, line in pmonitor( popens ):
-        #if host:
-            #print "<%s>: %s" % ( host.name, line.strip() )
-            #subnet1[-1].cmd('ping ', subnet2[-1].IP())        
-    time.sleep(10)
-    net.iperf((subnet1[-1],subnet2[-1]),seconds=30)
+    print('%s connections generated, %s flows generated ' %(count,count*2))
+    time.sleep(3)
+
+    iperf_outputs = {}
+
+    for i in range(1,num_iperf+1):
+        subnet1[-i].popen('iperf -s')
+        time.sleep(1)
+        iperf = subnet2[-i].popen('iperf -T20 -c ' + subnet1[-i].IP())
+        iperf_outputs[subnet2[-i]] = iperf
+        
+    for host, line in pmonitor( iperf_outputs ):
+        if host:
+            print "<%s>: %s" % ( host.name, line.strip() )
+
+    for host in subnet1:
+        host.cmd('pkill ping')
+
+    for host in subnet2:
+        host.cmd('pkill ping')
+
+    for i in range(1,num_iperf+1):
+        subnet2[-i].cmd('pkill iperf')
     net.stop()
 
 if __name__ == '__main__':
     #max_flow_test(112)
     #max_flow_test(128)
-    max_flow_test(2)
+    max_flow_test(200,num_iperf = 1)
